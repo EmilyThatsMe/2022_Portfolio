3f8fe38366704ef024d9bc17d134493e2c69db48
EmilyT <emilython@hotmail.com>
2022-03-31 14:17:34 -0500

    added about section and about section styles

--- client/src/components/About/index.js
+++ client/src/components/About/index.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import Image from '../../assets/images/Me.png';
 
 function About() {
   return (
-    <section>
-      <h1 id="about">Who am I?</h1>
+    <section className="about--container" id="about">
+      <div className="about--text">
+        <h1 className="about--header">Hi, I'm Emily!</h1>
+        <h2 className="about--h2">
+          I'm a web Developer based in the Twin Cities, MN.
+          <br />
+          Interested in working with me? Contact me Here!
+        </h2>
+      </div>
+      <div className="about--img--container">
+        <img className="about--img" src={Image}></img>
+      </div>
+      <div className="about--end">
+        <h2>That's Me!</h2>
+      </div>
     </section>
   );
 }
